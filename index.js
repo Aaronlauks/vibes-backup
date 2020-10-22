d02d9cd321a7a1838ba76a66c3c56c3e470d4271
aaronlauks <aaronlauks01@gmail.com>
2020-10-22 17:52:34 +0800

    ok

--- index.js
+++ index.js
@@ -66,12 +66,13 @@ bot.on('message', async message => {
       } else {
         command = bot.commands.get(bot.aliases.get(cmd));
       }
-      if(cooldown.has(message.guild.id) && cooldown.get(message.guild.id)[0] > 2){
-        message.channel.send(`🆒 Chill it with the commands!`)
-      } else if(cooldown.has(message.guild.id)){
+      let gay = ["accf", "acnl", "acnh", "acgcn", "reload"]
+      if(cooldown.has(message.guild.id)){
+        message.channel.send(`🆒 Chill it with the commands!`).then(m => m.delete(2000));
+      } else if(cooldown.has(message.guild.id)  && cooldown.get(message.guild.id)[0] == 2 && command.name.includes(gay)){
         cooldown.get(message.guild.id)[0]++;
         command.run(bot, message, args);
-      } else {
+      } else if(cooldown.get(message.guild.id)[0] == 2 && command.name.includes(gay)){
         cooldown.set(message.guild.id, new Array());
         cooldown.get(message.guild.id).push(1);
         command.run(bot, message, args);
