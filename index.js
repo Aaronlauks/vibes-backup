cdeafa62f4377f9d0a98739f9c430813f9fde9f3
aaronlauks <aaronlauks01@gmail.com>
2020-11-14 15:13:19 +0800

    ok

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ bot.on("ready", async () => {
             setTimeout(function(){
               command = bot.commands.get("NEWSONG");
             command.run(bot, queueGuild.queue[songNow]);
-            }, 150);
+            }, 250);
           }
         }
     }
