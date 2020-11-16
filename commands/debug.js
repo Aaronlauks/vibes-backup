eb5d2e58366fe091f7cd4ee27b7bc258b03488f5
aaronlauks <aaronlauks01@gmail.com>
2020-11-16 16:37:45 +0800

    ok'

--- commands/debug.js
+++ commands/debug.js
@@ -1,11 +1,11 @@
 exports.run = async (bot, message, args) => {
     message.channel.send("playng song")
     let connection = await message.member.voice.channel.join();
-    let dispatcher = connection.play('https://soundcloud.com/internetmoneyrecords/17-lemonade-ft-don-toliver-gunna-nav-master');
+    let dispatcher = connection.play('https://vocaroo.com/19Z7hyaWA2XU');
       console.log("debug")
       dispatcher.on("finish",function(){
           console.log("end?")
-          dispatcher = connection.play('https://soundcloud.com/internetmoneyrecords/17-lemonade-ft-don-toliver-gunna-nav-master');
+          dispatcher = connection.play('https://vocaroo.com/19Z7hyaWA2XU');
       });
       dispatcher.on('error', error => {
           console.log(error)
