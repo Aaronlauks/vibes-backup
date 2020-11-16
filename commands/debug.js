e304e6321a828849ba04f91258d6fb57f477d5c3
aaronlauks <aaronlauks01@gmail.com>
2020-11-16 16:43:41 +0800

    ok'

--- commands/debug.js
+++ commands/debug.js
@@ -1,11 +1,11 @@
 exports.run = async (bot, message, args) => {
     message.channel.send("playng song")
     let connection = await message.member.voice.channel.join();
-    let dispatcher = connection.play('../Music/ACCF/1AM.mp3');
+    let dispatcher = connection.play('./Music/ACCF/1AM.mp3');
       console.log("debug")
       dispatcher.on("finish",function(){
           console.log("end?")
-          dispatcher = connection.play('../Music/ACCF/1AM.mp3');
+          dispatcher = connection.play('./Music/ACCF/1AM.mp3');
       });
       dispatcher.on('error', error => {
           console.log(error)
