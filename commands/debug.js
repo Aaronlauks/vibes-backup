4f9594e16a190d93c7dc2b8e65b583bb695eba30
aaronlauks <aaronlauks01@gmail.com>
2020-11-16 15:34:51 +0800

    ok'

--- commands/debug.js
+++ commands/debug.js
@@ -1,11 +1,11 @@
 exports.run = async (bot, message, args) => {
     message.channel.send("playng song")
     let connection = await message.member.voice.channel.join();
-    let dispatcher = connection.play('https://cdn.discordapp.com/attachments/777785367607771146/777785471065391144/1AM.mp3');
+    let dispatcher = connection.play('https://soundcloud.com/internetmoneyrecords/17-lemonade-ft-don-toliver-gunna-nav-master');
       console.log("debug")
       dispatcher.on("finish",function(){
           console.log("end?")
-          dispatcher = connection.play('https://cdn.discordapp.com/attachments/777785367607771146/777785471065391144/1AM.mp3');
+          dispatcher = connection.play('https://soundcloud.com/internetmoneyrecords/17-lemonade-ft-don-toliver-gunna-nav-master');
       });
       dispatcher.on('error', error => {
           console.log(error)
