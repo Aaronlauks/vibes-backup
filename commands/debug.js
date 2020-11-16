4f5d9c16556304304e2c21f180eb6e807258643a
aaronlauks <aaronlauks01@gmail.com>
2020-11-16 14:56:45 +0800

    ok'

--- commands/debug.js
+++ commands/debug.js
@@ -1,11 +1,11 @@
 exports.run = async (bot, message, args) => {
     message.channel.send("playng song")
     let connection = await message.member.voice.channel.join();
-    const dispatcher = connection.play('http://www.sample-videos.com/audio/mp3/wave.mp3');
+    let dispatcher = connection.play('http://www.sample-videos.com/audio/mp3/wave.mp3');
       console.log("debug")
       dispatcher.on("finish",function(){
           console.log("end?")
-          connection.play('http://www.sample-videos.com/audio/mp3/wave.mp3');
+          dispatcher = connection.play('http://www.sample-videos.com/audio/mp3/wave.mp3');
       });
       dispatcher.on('error', error => {
           console.log(error)
