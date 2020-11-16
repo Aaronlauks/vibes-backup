9893786c9a09314508e286ddb1aef3e1a7d185e0
aaronlauks <aaronlauks01@gmail.com>
2020-11-16 14:07:53 +0800

    ok'

--- commands/accf.js
+++ commands/accf.js
@@ -9,57 +9,30 @@ exports.run = async (bot, message, args) => {
               queueGuild.queue = [`${message.guild.id}`]
           } else if(!queueGuild.queue.includes(message.guild.id))queueGuild.queue.push(message.guild.id)
           await queueGuild.save().catch(e => console.log(e));
-        let selectTime = 0;
-        if(args[0]){
-            let argsArgs = args[0].split("");
-        if(argsArgs.length > 2){
-            if(argsArgs[argsArgs.length - 2].toUpperCase() + argsArgs[argsArgs.length - 1].toUpperCase() == "PM"){
-                    argsArgs.splice(argsArgs.length - 1, 1);
-                    argsArgs.splice(argsArgs.length - 1, 1);
-                    let newArgs = argsArgs.join("");
-                    if(newArgs < 1 || newArgs > 12) {
-                        let queueChannel = await queueVoice.findOne({
-                            guildID: message.guild.id
-                          });
-                          queueChannel.running = false;
-                          await queueChannel.save().catch(e => console.log(e));
-                        return message.channel.send(`<:xcross:690880230562201610> not a valid time lol`);
-                    }
-                    if(newArgs == 12) newArgs -= 12;
-                    selectTime = newArgs - new Date().getHours() + 12;
-            } else if(argsArgs[argsArgs.length - 2].toUpperCase() + argsArgs[argsArgs.length - 1].toUpperCase() == "AM"){
-                argsArgs.splice(argsArgs.length - 1, 1);
-                argsArgs.splice(argsArgs.length - 1, 1);
-                let newArgs = argsArgs.join("");
-                if(newArgs < 1 || newArgs > 12) {
-                    let queueChannel = await queueVoice.findOne({
-                        guildID: message.guild.id
-                      });
-                      queueChannel.running = false;
-                      await queueChannel.save().catch(e => console.log(e));
-                    return message.channel.send(`<:xcross:690880230562201610> not a valid time lol`);
+
+          let selectTime;
+          let time;
+          let error = false;
+          if(!args[0]){
+            selectTime = 0;
+          } else {
+            if(isNaN(args[0]) && (args[0].includes("AM") || args[0].includes("PM"))){
+                if(args[0].includes("AM")){
+                  time = args[0].replace("AM", "");
+                  if(time > 0 && time < 13){
+                    selectTime = new Date().getHours() - time;
+                    time = args[0];
+                  } else error = true;
+                } else {
+                  time = args[0].replace("AM", "");
+                  if(time > 0 && time < 13){
+                    selectTime = new Date().getHours() - time;
+                    time = args[0];
+                  } else error = true;
                 }
-                if(newArgs == 12) newArgs = 12 + parseInt(newArgs);
-                selectTime = newArgs - new Date().getHours();
-            } else {
-                let queueChannel = await queueVoice.findOne({
-                    guildID: message.guild.id
-                  });
-                  queueChannel.running = false;
-                  await queueChannel.save().catch(e => console.log(e));
-                return message.channel.send(`<:xcross:690880230562201610> not a valid time lol`);
-            }
-        } else if(args[0] > 0 && args[0] < 25) {
-            selectTime = args[0] - new Date().getHours();
-        } else {
-            let queueChannel = await queueVoice.findOne({
-                guildID: message.guild.id
-              });
-              queueChannel.running = false;
-              await queueChannel.save().catch(e => console.log(e));
-            return message.channel.send(`<:xcross:690880230562201610> not a valid time lol`);
-        }
-        }
+              } else error = true;
+          }
+
         let stop = false;
         let connection = await message.member.voice.channel.join().catch(e => {
             message.channel.send(`<:xcross:690880230562201610> Couldn't connect to voice channel!`)
@@ -76,46 +49,6 @@ exports.run = async (bot, message, args) => {
             queueChannel.songType = "Animal Crossing **City Folk**";
             queueChannel.voiceID = message.member.voice.channel.id;
             queueChannel.running = false;
-        }
-        let time;
-        if(selectTime != 0){
-            time = new Date().getHours() + +selectTime;
-          } else {
-            time = new Date().getHours();
-          }
-          if(time < 13){
-            if(time < 0){
-              time = parseInt(time) + 12;
-              if(time == 12) {
-                time += "AM";
-              } else {
-                time += "PM";
-              }
-            } else {
-            if(time == 12) {
-              time += "PM";
-            } else if(time == 0) {
-              time = "AM";
-            } else {
-              time += "12AM";
-            }
-          }
-        } else {
-            time = time - 12;
-            if(time > 12){
-              time = time - 12;
-            if(time == 12) {
-              time += "PM";
-            } else {
-              time += "AM";
-            }
-          } else {
-          if(time == 12) {
-            time += "AM";
-          } else {
-            time += "PM";
-          }
-        }
         }
         const dispatcher = await connection.play(`../music/ACCF/${time}.mp3`)
         console.log(`../music/ACCF/${time}.mp3`)
