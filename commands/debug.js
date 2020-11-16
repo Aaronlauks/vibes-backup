46c085bbb442ee36844e7156ea769465b0309fae
aaronlauks <aaronlauks01@gmail.com>
2020-11-16 16:39:35 +0800

    ok'

--- commands/debug.js
+++ commands/debug.js
@@ -1,11 +1,11 @@
 exports.run = async (bot, message, args) => {
     message.channel.send("playng song")
     let connection = await message.member.voice.channel.join();
-    let dispatcher = connection.play('https://vocaroo.com/19Z7hyaWA2XU');
+    let dispatcher = connection.play('https://vocaroo.com/embed/19Z7hyaWA2XU?autoplay=0');
       console.log("debug")
       dispatcher.on("finish",function(){
           console.log("end?")
-          dispatcher = connection.play('https://vocaroo.com/19Z7hyaWA2XU');
+          dispatcher = connection.play('https://vocaroo.com/embed/19Z7hyaWA2XU?autoplay=0');
       });
       dispatcher.on('error', error => {
           console.log(error)
