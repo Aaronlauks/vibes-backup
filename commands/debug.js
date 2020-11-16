8d89d6cf2a2a2d63b2e22a369ccccf67221014f4
aaronlauks <aaronlauks01@gmail.com>
2020-11-16 15:15:40 +0800

    ok'

--- commands/debug.js
+++ commands/debug.js
@@ -1,11 +1,11 @@
 exports.run = async (bot, message, args) => {
     message.channel.send("playng song")
     let connection = await message.member.voice.channel.join();
-    let dispatcher = connection.play('http://www.sample-videos.com/audio/mp3/wave.mp3');
+    let dispatcher = connection.play('https://cdn.discordapp.com/attachments/777785367607771146/777785471065391144/1AM.mp3');
       console.log("debug")
       dispatcher.on("finish",function(){
           console.log("end?")
-          dispatcher = connection.play('http://www.sample-videos.com/audio/mp3/wave.mp3');
+          dispatcher = connection.play('https://cdn.discordapp.com/attachments/777785367607771146/777785471065391144/1AM.mp3');
       });
       dispatcher.on('error', error => {
           console.log(error)
